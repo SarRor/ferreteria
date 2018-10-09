28c27b61cb7e28940eded81371550fd22a9f4e91
salvador aguirre <salvador.aguirro@gmail.com>
2018-10-09 02:58:01 -0500

    11c

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def inicio
   end
+  def productos
+  end
+  def quienes_somos
+  end
+  def contacto
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'home#inicio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'productos', to: "home#productos"
+  get 'quienes_somos', to: "home#quienes_somos"
+  get 'contacto', to: "home#contacto"
 end
